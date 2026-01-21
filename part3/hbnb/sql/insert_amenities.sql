345651834831afd074818de3f7d17a5841c5669d
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-21 10:37:37 +0300

    Update insert_amenities.sql

--- part3/hbnb/sql/insert_amenities.sql
+++ part3/hbnb/sql/insert_amenities.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO amenities (id, name, created_at, updated_at)
+VALUES
+    (UUID(), 'WiFi', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (UUID(), 'Swimming Pool', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (UUID(), 'Air Conditioning', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
