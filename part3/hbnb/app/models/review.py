b113656bb5c765d6eb9c8bd77c43357ff2e07ead
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-27 16:33:15 +0300

    Correct Review class syntax in review.py
    Fix syntax error in Review class definition.

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -1,8 +1,2 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
-from app.models.base_model import Base, BaseModel
-
-class Review(Base, BaseModel):
-    __tablename__ = "reviews"
-    text 
-        }
